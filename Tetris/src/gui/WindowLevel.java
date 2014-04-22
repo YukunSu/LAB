acf1a8d535faef642dc6698d5ba693d78a983107
YukunSu <yu.k.su@mail.mcgill.ca>
2014-04-22 01:12:38 -0400

    Add more pictures

--- Tetris/src/gui/WindowLevel.java
+++ Tetris/src/gui/WindowLevel.java
@@ -16,7 +16,7 @@ public class WindowLevel extends Window {
 
     private static final int LOCAL_RECORD_WIDTH = IMAGE_LOCAL_RECORD.getWidth(null);
 
-    private static final int OFFSET = 6;
+    private static final int OFFSET = LOCAL_RECORD_WIDTH >> 1;
 
     public WindowLevel(int x, int y, int w, int h) {
         super(x, y, w, h);
