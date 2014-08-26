084ffc82387f025bdf085ac3501376a7cc66bd60
YukunSu <yu.k.su@mail.mcgill.ca>
2014-08-25 23:31:02 -0400

    Add more methods in Window.java

--- Tetris/src/gui/Window.java
+++ Tetris/src/gui/Window.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 
@@ -20,18 +22,29 @@ public abstract class Window {
     ConfigGame gameConfig = ConfigFactory.getConfigGame();
     protected int windowBorderWidth = gameConfig.getWindowBorderSize();
     protected int DistanceTitle = gameConfig.getPadding();
-    protected Image windowImage = new ImageIcon("Graphics/Windows/window02.png").getImage();
-    protected static final Image IMAGE_NUMBER = new ImageIcon("Graphics/Words/num02.png").getImage();
-    protected static final int NUMBER_WIDTH = IMAGE_NUMBER.getWidth(null) / 10;
-    protected static final int NUMBER_HEIGHT = IMAGE_NUMBER.getHeight(null);
+    protected static final int NUMBER_WIDTH = Images.NUMBER.getWidth(null) / 10;
+    protected static final int NUMBER_HEIGHT = Images.NUMBER.getHeight(null);
+    private static final Font CALIBRI_FONT = new Font("Calibri", Font.BOLD, 20);
+    /**
+     * Initialize height of experience bar picture
+     */
+    protected static final int RECTANGLE_HEIGHT = Images.RECTANGLE.getHeight(null);
+    /**
+     * Initialize width of experience bar picture
+     */
+    private static final int RECTANGLE_WIDTH = Images.RECTANGLE.getWidth(null);
+    /**
+     * Initialize width of experience bar
+     */
+    private final int expWidth;
     /**
      * Window picture width
      */
-    protected int windowWidth = windowImage.getWidth(null);
+    protected int windowWidth = Images.WINDOW.getWidth(null);
     /**
      * Window picture height
      */
-    protected int window_height = windowImage.getHeight(null);
+    protected int windowHeight = Images.WINDOW.getHeight(null);
     /**
      * X coordinate of window's upper left corner
      */
@@ -58,16 +71,17 @@ public abstract class Window {
         this.y = y;
         this.width = w;
         this.height = h;
+        expWidth = this.width - (DistanceTitle << 1);
     }
 
     protected void setWindowBorderWidth(int border) {
         windowBorderWidth = border;
     }
 
-    protected void setWindowImage(String picNameExt) {
-        windowImage = new ImageIcon("Graphics/Windows/" + picNameExt).getImage();
-        windowWidth = windowImage.getWidth(null);
-        window_height = windowImage.getHeight(null);
+    protected void setwindowImage(String picNameExt) {
+        Images.WINDOW = new ImageIcon("Graphics/Windows/" + picNameExt).getImage();
+        windowWidth = Images.WINDOW.getWidth(null);
+        windowHeight = Images.WINDOW.getHeight(null);
     }
 
     protected void setDistanceTitle(int distance) {
@@ -81,49 +95,49 @@ public abstract class Window {
      */
     protected void printWindowBorder(Graphics g) {
         // Draw upper left
-        g.drawImage(windowImage, x, y, x + windowBorderWidth, y
+        g.drawImage(Images.WINDOW, x, y, x + windowBorderWidth, y
                 + windowBorderWidth, ZERO, ZERO, windowBorderWidth,
                 windowBorderWidth, null);
         // Draw upper middle
-        g.drawImage(windowImage, x + windowBorderWidth, y, x + width
+        g.drawImage(Images.WINDOW, x + windowBorderWidth, y, x + width
                 - windowBorderWidth, y + windowBorderWidth, windowBorderWidth,
                 ZERO, windowWidth - windowBorderWidth, windowBorderWidth, null);
         // Draw upper right
-        g.drawImage(windowImage, x + width - windowBorderWidth, y, x + width, y
+        g.drawImage(Images.WINDOW, x + width - windowBorderWidth, y, x + width, y
                 + windowBorderWidth, windowWidth - windowBorderWidth, ZERO,
                 windowWidth, windowBorderWidth, null);
 
         // Draw middle left
-        g.drawImage(windowImage, x, y + windowBorderWidth, x
+        g.drawImage(Images.WINDOW, x, y + windowBorderWidth, x
                 + windowBorderWidth, y + height - windowBorderWidth, ZERO,
-                windowBorderWidth, windowBorderWidth, window_height
+                windowBorderWidth, windowBorderWidth, windowHeight
                         - windowBorderWidth, null);
         // Draw middle middle
-        g.drawImage(windowImage, x + windowBorderWidth, y + windowBorderWidth,
+        g.drawImage(Images.WINDOW, x + windowBorderWidth, y + windowBorderWidth,
                 x + width - windowBorderWidth, y + height - windowBorderWidth,
                 windowBorderWidth, windowBorderWidth, windowWidth
-                        - windowBorderWidth, window_height - windowBorderWidth,
+                        - windowBorderWidth, windowHeight - windowBorderWidth,
                 null);
         // Draw middle right
-        g.drawImage(windowImage, x + width - windowBorderWidth, y
+        g.drawImage(Images.WINDOW, x + width - windowBorderWidth, y
                 + windowBorderWidth, x + width, y + height - windowBorderWidth,
                 windowWidth - windowBorderWidth, windowBorderWidth,
-                windowWidth, window_height - windowBorderWidth, null);
+                windowWidth, windowHeight - windowBorderWidth, null);
 
         // Draw lower left
-        g.drawImage(windowImage, x, y + height - windowBorderWidth, x
-                + windowBorderWidth, y + height, ZERO, window_height
-                - windowBorderWidth, windowBorderWidth, window_height, null);
+        g.drawImage(Images.WINDOW, x, y + height - windowBorderWidth, x
+                + windowBorderWidth, y + height, ZERO, windowHeight
+                - windowBorderWidth, windowBorderWidth, windowHeight, null);
         // Draw lower middle
-        g.drawImage(windowImage, x + windowBorderWidth, y + height
+        g.drawImage(Images.WINDOW, x + windowBorderWidth, y + height
                 - windowBorderWidth, x + width - windowBorderWidth, y + height,
-                windowBorderWidth, window_height - windowBorderWidth,
-                windowWidth - windowBorderWidth, window_height, null);
+                windowBorderWidth, windowHeight - windowBorderWidth,
+                windowWidth - windowBorderWidth, windowHeight, null);
         // Draw lower right
-        g.drawImage(windowImage, x + width - windowBorderWidth, y + height
+        g.drawImage(Images.WINDOW, x + width - windowBorderWidth, y + height
                 - windowBorderWidth, x + width, y + height, windowWidth
-                - windowBorderWidth, window_height - windowBorderWidth,
-                windowWidth, window_height, null);
+                - windowBorderWidth, windowHeight - windowBorderWidth,
+                windowWidth, windowHeight, null);
     }
 
     public void setDto(GameDto dto){
@@ -144,13 +158,63 @@ public abstract class Window {
             if(maxBit - i <= num.length()){
                 int index = i - maxBit + num.length();
                 int singleNum = num.charAt(index) - '0';
-                g.drawImage(IMAGE_NUMBER, 
+                g.drawImage(Images.NUMBER, 
                         this.x + x + NUMBER_WIDTH * i, this.y + y, 
                         this.x + x + NUMBER_WIDTH * (i + 1), this.y + y + NUMBER_HEIGHT,
                         singleNum * NUMBER_WIDTH, 0, 
                         (singleNum + 1) * NUMBER_WIDTH, NUMBER_HEIGHT, null);
             }
         }
+    }
+
+    /**
+     * Draw the exp bar based on current score and level
+     * @param title Name displayed for the rectangle bar (players' names or title)
+     * @param playerScores
+     * @param recPositionY Starting Y position of the rectangle
+     * @param linesRatio The amount of lines left to level up / The total amount of lines needed to level up
+     * @param g The pen
+     */
+    protected void drawRectangle(String title, String playerScores, int recPositionY, double linesRatio, Graphics g) {
+        // Initialization
+        int rectX = this.x + DistanceTitle;
+        int rectY = this.y + recPositionY;
+        // Display background of rectangle bar
+        g.setColor(Color.BLACK);
+        g.fillRect(rectX, rectY, expWidth, RECTANGLE_HEIGHT + 4);
+        g.setColor(Color.WHITE);
+        g.fillRect(rectX + 1, rectY + 1, expWidth - 2, RECTANGLE_HEIGHT + 2);
+        g.setColor(Color.BLACK);
+        g.fillRect(rectX + 2, rectY + 2, expWidth - 4, RECTANGLE_HEIGHT);
+
+        // Calculate width
+        int experienceWidth = (int)((linesRatio * (expWidth - 4)));
+        // Calculate color
+        int picIndex = (int)(linesRatio * RECTANGLE_WIDTH) - 1;
+        g.drawImage(Images.RECTANGLE,
+                rectX + 2, rectY + 2,
+                rectX + 2 + experienceWidth, rectY + 2 + RECTANGLE_HEIGHT,
+                picIndex, 0, picIndex + 1, RECTANGLE_HEIGHT, null);
+
+        // Display string
+        g.setColor(Color.WHITE);
+        g.setFont(CALIBRI_FONT);
+        g.drawString(title, rectX + 7, rectY + 22);
+
+        if (playerScores != null) {
+            //TODO config
+            int maxBit = 5;
+            int leftPad = (maxBit - playerScores.length()) * 10;
+            g.drawString(playerScores, leftPad + rectX + 240, rectY + 22);
+        }
+    }
+
+    protected void drawImageAtCenter(Image img, Graphics g){
+        int imageWidth = img.getWidth(null);
+        int imageHeight = img.getHeight(null);
+        int x = this.x + (this.width - imageWidth >> 1);
+        int y = this.y + (this.height - imageHeight >> 1);
+        g.drawImage(img, x, y, null);
     }
 
     /**
