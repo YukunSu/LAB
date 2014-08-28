01711fcd63c075687fbf623a422f61906b0a78c8
YukunSu <yu.k.su@mail.mcgill.ca>
2014-08-28 01:32:55 -0400

    Can change bg now

--- Tetris/src/gui/WindowBackground.java
+++ Tetris/src/gui/WindowBackground.java
@@ -12,16 +12,14 @@ import javax.swing.ImageIcon;
  */
 public class WindowBackground extends Window {
 
-    // TODO temp background
-    private Image imageBackground = new ImageIcon("Graphics/Background/01.jpg").getImage();
-
     public WindowBackground(int x, int y, int w, int h) {
         super(x, y, w, h);
     }
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(imageBackground, ZERO, ZERO, null);
+        int backgroundIndex = this.dto.getLevel() % Images.NEXT_BACKGROUND_LIST.size();
+        g.drawImage(Images.NEXT_BACKGROUND_LIST.get(backgroundIndex), ZERO, ZERO, null);
     }
 
 }
