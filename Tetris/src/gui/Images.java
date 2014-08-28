4633f08bc5c0290e8c48d9c54e509fceccd61617
YukunSu <yu.k.su@mail.mcgill.ca>
2014-08-27 23:42:56 -0400

    Add shadow

--- Tetris/src/gui/Images.java
+++ Tetris/src/gui/Images.java
@@ -1,6 +1,9 @@
 package gui;
 
 import java.awt.Image;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 
@@ -56,6 +59,11 @@ public class Images {
     public static Image SCORES = new ImageIcon("Graphics/Words/point.png").getImage();
 
     /**
+     * Shadow picture
+     */
+    public static Image SHADOW = new ImageIcon("Graphics/Images/shadow2.png").getImage();
+
+    /**
      * Signature picture
      */
     public static Image SIGNATUR = new ImageIcon("Graphics/Words/signature.png").getImage();
@@ -66,14 +74,30 @@ public class Images {
     public static Image WINDOW = new ImageIcon("Graphics/Windows/window02.png").getImage();
 
     /**
+     * Pictures for backgrounds
+     */
+    public static List<Image> NEXT_BACKGROUND_LIST;
+
+    /**
      * Pictures for each type of tetris
      */
     public static Image[] NEXT_ACT;
 
     static{
+        // TODO hard code
+        // Next tetris picture
         NEXT_ACT = new Image[7];
         for (int i = 0; i < NEXT_ACT.length; i++) {
             NEXT_ACT[i] = new ImageIcon("Graphics/Images/" + i + ".png") .getImage();
+        }
+        // Next background picture
+        File directory = new File("Graphics/Background");
+        File[] files = directory.listFiles();
+        NEXT_BACKGROUND_LIST = new ArrayList<Image>();
+        for (File file : files) {
+            if (!file.isDirectory()) {
+                NEXT_BACKGROUND_LIST.add(new ImageIcon(file.getPath()).getImage());
+            }
         }
     }
 }
--- Tetris/src/gui/WindowGame.java
+++ Tetris/src/gui/WindowGame.java
@@ -12,6 +12,8 @@ public class WindowGame extends Window {
 
     // TODO config
     private static final int SIZE_ROL = 5;
+    private static final int LEFT_LIMIT = 0;
+    private static final int RIGHT_LIMIT = 9;
 
     public WindowGame(int x, int y, int w, int h) {
         super(x, y, w, h);
@@ -20,6 +22,9 @@ public class WindowGame extends Window {
     public void paint(Graphics g) {
         this.printWindowBorder(g);
         Point[] points = this.dto.getGameAct().getActPoints();
+        // Paint shadow
+        // TODO shadow on / off
+        this.drawShadow(points, true, g);
         //Get tetris type code
         int typeCode = this.dto.getGameAct().getTypeCode();
         //Print tetris
@@ -29,13 +34,41 @@ public class WindowGame extends Window {
 
         //Print map
         boolean[][] gameMap = this.dto.getGameMap();
+        int imageIndex = 0;
+        int lvl = this.dto.getLevel();
+        imageIndex = lvl == 0 ? 0 : (lvl - 1) % 7 + 1;
+        // TODO lost imageIndex = 8
         for (int x = 0; x < gameMap.length; x++) {
             for (int y = 0; y < gameMap[x].length; y++) {
                 if(gameMap[x][y]){
-                    drawTetris(x, y, 0, g);
+                    drawTetris(x, y, imageIndex, g);
                 }
             }
         }
+    }
+
+    /**
+     * Draw shadow for each tetris
+     * @param points
+     * @param isShadowDisplayed
+     * @param g
+     */
+    private void drawShadow(Point[] points, boolean isShadowDisplayed, Graphics g) {
+        if (!isShadowDisplayed) {
+            return;
+        }
+        // TODO hard code
+        int leftMostX = RIGHT_LIMIT;
+        int rightMostX = LEFT_LIMIT;
+        for (Point point : points) {
+            leftMostX = point.x < leftMostX ? point.x : leftMostX;
+            rightMostX = point.x > rightMostX ? point.x : rightMostX;
+        }
+        g.drawImage(Images.SHADOW, 
+                this.x + BORDER + (leftMostX << SIZE_ROL) - 7,
+                this.y + BORDER - 7,
+                (rightMostX - leftMostX +1) << SIZE_ROL,
+                this.height - (BORDER << 1) + 14, null);
     }
 
     /**
